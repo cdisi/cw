b9b77085d39ff2fcac2330f8f39d102fca91b6d1
zihnikaya <zihnikaya@gmail.com>
2016-08-10 16:59:39 +0300

    u

--- src/main/java/com/zk/cw/yeni_cihaz/YeniCihazMainWindow.java
+++ src/main/java/com/zk/cw/yeni_cihaz/YeniCihazMainWindow.java
@@ -2,9 +2,16 @@ package com.zk.cw.yeni_cihaz;
 
 import java.awt.Color;
 
+import javax.swing.Action;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+
+import com.zk.cw.cihaz_url.CihazAraAction;
+import com.zk.cw.uretici.UreticilerAction;
 
 public class YeniCihazMainWindow {
 	
@@ -18,11 +25,47 @@ public class YeniCihazMainWindow {
 	    return INSTANCE;
 	}
 	
-	public void buildGui(JFrame mainFrame){
+	public void buildGui(JFrame mainFrame, JMenuBar menuBar){
 		yeniCihazTableModel = new YeniCihazTableModel();
 		yeniCihazTable = new JTable(yeniCihazTableModel);
+		buildActionsAndMenu(mainFrame, menuBar);
 		buildContent(mainFrame);
 	}	
+	
+	  /** Build the menu bar. */
+	  
+	private void buildActionsAndMenu(JFrame aFrame, JMenuBar menuBar) {
+		menuBar.removeAll();
+		menuBar.revalidate(); 
+		menuBar.repaint();
+		
+		JMenu fileMenu = new JMenu("File");
+	    fileMenu.setMnemonic('F'); 
+
+	    Action ureticilerAction = new UreticilerAction(aFrame);
+	    fileMenu.add(new JMenuItem(ureticilerAction));
+
+	    Action cihazAraAction = new CihazAraAction(aFrame);
+	    fileMenu.add(new JMenuItem(cihazAraAction));
+		    
+	    Action yeniCihazAction = new YeniCihazAction(aFrame, menuBar);
+	    fileMenu.add(new JMenuItem(yeniCihazAction));
+		    
+	    Action exitAction = new ExitAction();
+	    fileMenu.add(new JMenuItem(exitAction));
+	    menuBar.add(fileMenu);
+		    
+	    JMenu editMenu = new JMenu("Edit");
+	    editMenu.setMnemonic('E');
+		    
+	    JMenu helpMenu = new JMenu("Help");
+	    helpMenu.setMnemonic('H');
+	    helpMenu.add(new JMenuItem(new AboutAction(aFrame)));
+	    menuBar.add(helpMenu);    
+		    
+	    aFrame.setJMenuBar(menuBar);
+	
+	  }	
 	
 	private void buildContent(JFrame mainFrame) {	    
 		yeniCihazTable.setBackground(Color.LIGHT_GRAY);
